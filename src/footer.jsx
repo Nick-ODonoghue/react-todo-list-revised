915ce165306f7a493e49f49936a0b0f3b60ad470
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-06-11 13:02:23 +0100

    built & styled todo component

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,10 +3,10 @@ const Footer = () => {
 		<>
 			<h3 className=" text-center text-sm">
 				Created by
-				<a href="#_" class="relative px-1 py-1 group ml-2">
-					<span class="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 -translate-y-1 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0 rounded-md"></span>
-					<span class="absolute inset-0 w-full h-full border-2 border-black rounded-md"></span>
-					<span class="relative font-medium">NickOD.xyz</span>
+				<a href="#_" className="relative px-1 py-1 group ml-2">
+					<span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 -translate-y-1 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0 rounded-md"></span>
+					<span className="absolute inset-0 w-full h-full border-2 border-black rounded-md"></span>
+					<span className="relative font-medium">NickOD.xyz</span>
 				</a>
 			</h3>
 		</>
--- src/todo.jsx
+++ src/todo.jsx
@@ -1,9 +1,27 @@
 const Todo = ({ todos }) => {
 	return (
 		<>
-			{todos.map((todo) => {
-				return <p>{todo.title}</p>;
-			})}
+			<h2 className=" text-center font-semibold text-lg mt-2 mb-6">ToDo List</h2>
+			{todos.length === 0 && <p className="text-center font-medium text-lg">No ToDo's Added</p>}
+			<ul>
+				{todos.map((todo) => {
+					return (
+						<li key={todo.id} className="flex justify-between px-4 my-4 max-w-sm mx-auto">
+							<div className=" flex gap-4">
+								<input type="checkbox" checked={todo.completed} />
+								<p className={`self-center font-medium ${todo.completed ? "line-through font-light" : ""}`}>
+									{todo.title}
+								</p>
+							</div>
+							<button className="relative px-1 py-1 group ml-2 hover:text-white transition duration-300">
+								<span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 -translate-y-1 bg-red-500 group-hover:translate-x-0 group-hover:translate-y-0 rounded-md"></span>
+								<span className="absolute inset-0 w-full h-full border-2 border-black rounded-md"></span>
+								<span className="relative font-medium ">Delete</span>
+							</button>
+						</li>
+					);
+				})}
+			</ul>
 		</>
 	);
 };
